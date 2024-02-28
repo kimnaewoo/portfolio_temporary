e45062d2c3d521b7ce1654b2514d752e4ba53cbb
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2024-02-28 15:23:21 +0900

    포트폴리오 디자인 및 반응형 수정

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ function toggleMenu() {
   menu.classList.toggle('open');
   icon.classList.toggle('open');
 }
+
+const nav = document.querySelector('#desktop-nav');
+const ham = document.querySelector('#hamburger-nav');
+
+const navHeight = nav.getBoundingClientRect().height;
+const hamHeight = ham.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  const scrollY = window.scrollY;
+
+  navHeight < scrollY ? nav.classList.add('fixed') : nav.classList.remove('fixed');
+  hamHeight < scrollY ? ham.classList.add('fixed') : ham.classList.remove('fixed');
+
+  if (scrollY > navHeight) {
+    nav.classList.add('fixed');
+  } else {
+    nav.classList.remove('fixed');
+  }
+  if (scrollY > hamHeight) {
+    ham.classList.add('fixed');
+  } else {
+    ham.classList.remove('fixed');
+  }
+});
